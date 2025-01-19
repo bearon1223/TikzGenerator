3d66d0370619cfe19c78e6daf5a4b8d32d26c0a1
Aron Magnus <aronmagnususa@gmail.com>
2025-01-18 21:08:41 -0700

    Update Colors

--- core/src/main/java/com/tikz/ProgramState.java
+++ core/src/main/java/com/tikz/ProgramState.java
@@ -26,13 +26,13 @@ public class ProgramState {
     public static final ColorHolder[] colors = {
         new ColorHolder(new Color(0x000000ff), "Black"  , 1.0f),
         new ColorHolder(new Color(0x808080ff), "Gray"   , 1.0f),
-        new ColorHolder(new Color(0xd93122ff), "Red"    , 1.0f),
-        new ColorHolder(new Color(0x3716f5ff), "Blue"   , 1.0f),
-        new ColorHolder(new Color(0x8dfb4aff), "Green"  , 1.0f),
-        new ColorHolder(new Color(0xe18631ff), "Orange" , 1.0f),
-        new ColorHolder(new Color(0xf6eb5fff), "Yellow" , 1.0f),
-        new ColorHolder(new Color(0x70b7edff), "Cyan"   , 1.0f),
-        new ColorHolder(new Color(0xfc2f99ff), "Magenta", 1.0f),
+        new ColorHolder(new Color(0xff0101ff), "Red"    , 1.0f),
+        new ColorHolder(new Color(0x01ff01ff), "Green"  , 1.0f),
+        new ColorHolder(new Color(0x0101ffff), "Blue"   , 1.0f),
+        new ColorHolder(new Color(0xff8001ff), "Orange" , 1.0f),
+        new ColorHolder(new Color(0xffeb3dff), "Yellow" , 1.0f),
+        new ColorHolder(new Color(0x01b9f2ff), "Cyan"   , 1.0f),
+        new ColorHolder(new Color(0xfb3199ff), "Magenta", 1.0f),
     };
     public static ColorHolder selectedColor = colors[colorIndex];
 }
